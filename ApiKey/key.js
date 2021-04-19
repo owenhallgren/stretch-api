50a9074a54cf2392e6c1000ee77181c8cccaba83
JS <jeff.shep777@gmail.com>
2021-04-19 13:15:08 -0600

    Remove code comments

--- ApiKey/key.js
+++ ApiKey/key.js
@@ -1,7 +1,3 @@
-const key = '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'
-
-// export default key;
-// module.exports = key
 
 module.exports = {
   key: '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'
--- seeds/reviews.js
+++ seeds/reviews.js
@@ -1,37 +1,11 @@
 const reviewData = require ('../reviewdata')
 const userData = require ('../userdata')
-// exports.seed = function(knex) {
-//   // Deletes ALL existing entries
-//   return knex('table_name').del()
-//     .then(function () {
-//       // Inserts seed entries
-//       return knex('table_name').insert([
-//         {id: 1, colName: 'rowValue1'},
-//         {id: 2, colName: 'rowValue2'},
-//         {id: 3, colName: 'rowValue3'}
-//       ]);
-//     });
-// };
-
-// exports.seed = async function (knex) {
-//   try {
-//     await knex('reviews').del() // delete all footnotes first
-  
-//     // Now that we have a clean slate, we can re-insert our paper data
-//     // Insert a single paper, return the paper ID, insert 2 footnotes
-//     await knex('reviews').insert({
-//       username: 'jacksonmcguire', summary: 'this is a summary', email: 'lumbersexual69@gmail.com', language: 'javascript', date: '03/11/21', repo:'https://github.com/josharagon/self-care-center', status:'complete', reviewer: 'Jackson'
-//     }, 'id')
-//   } catch (error) {
-//     console.log(`Error seeding data: ${error}`);
-//   }
-// }
 
 
 exports.seed = async function (knex) {
   try {
     await knex('users').del()
-    await knex('reviews').del() // delete all footnotes first
+    await knex('reviews').del()
     
     await knex('reviews').insert(reviewData)
     await knex('users').insert(userData)

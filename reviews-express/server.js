a3cc8fb84d814d8d066a54ac0175f6870ae1f149
Owen Hallgren <owenhallgren@gmail.com>
2021-04-19 11:02:06 -0600

    Hid api key

--- reviews-express/server.js
+++ reviews-express/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const { default: knex } = require('knex');
-// const knex = require('./db/knex.js');
 const cors = require('cors');
 const { response } = require('express');
 app.use(express.json());
-
+const { key } = require('../ApiKey/key')
+const sgMail = require('@sendgrid/mail')
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile.js')[environment];
 const database = require('knex')(configuration);
@@ -33,20 +33,23 @@ app.get('/api/v1/users', async (request, response) => {
   }
 })
 
-// app.put('/api/v1/reviews/:id', async (request, response) => {
-//   try {
-//      const { id } = request.params
-//      await database('reviews').where({ id: id }).update({ name: 'Ceral'})
-//       // knex('reviews')
-//       //   .where({ id: id })
-//       //   .update({ name: 'Ceral'})
+app.post('/api/v1/email', (req, res) => {
+  sgMail.setApiKey(key)
 
-//       response.status(200).json({id: 'ceral'})
-//   } catch (error) {
-//     response.status(500).json({ error: 'ceral' })
-//   }
- 
-// })npm
+  const review = req.body;
+
+   let message = {
+      to: review.email,
+      from: 'PairedPear@gmail.com',
+      subject: `Your review on Pear has been ${review.type}`,
+      text:`Hi ${review.user}! ${review.username} has ${review.type} your review! If you wish to get in touch you can contact them at ${review.reviewerEmail}. You will be notified when any update has been made to your review`
+    }
+   
+    
+    sgMail.send(message)
+    .then(() => res.status(200).json({status: 'ok'}))
+    .catch(err => console.log('Error', err))
+})
 
 app.put('/api/v1/reviews/cancel/:id', (req, res) => {
     const { id } = req.params
@@ -116,84 +119,8 @@ app.delete('/api/v1/reviews/:id', (req, res) => {
 
 })
 
-// app.post('/messages', async (req, res) => {
-//   try {
-//     const message = await knex('messages').insert({
-//       message: req.body.message,
-//       user_name: req.body.user_name
-//     }, 'message')
-//     res.status(201).json( { message } )
-//   } catch (error) {
-//     res.status(500).json( { error } )
-//   }
-// })
-
-
-
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
 
 
-// //PUT request:  CANCEL button
-// app.put('/reviews/cancel/:id', (request, response) => {
-//   const { id } = request.params;
-
-//   app.locals.reviews.forEach((review) => {
-//     if(parseInt(id) === review.id) {
-//       review.status = '';
-//       review.reviewer = '';
-//     }
-//   })
-
-//   response.status(200).json(app.locals.reviews)
-// })
-
-// //PUT request:  UNDO button
-// app.put('/reviews/undo/:id', (request, response) => {
-
-//   const { id } = request.params;
-
-//   app.locals.reviews.forEach((review) => {
-//     if(parseInt(id) === review.id) {
-//       review.status = 'active';
-//     }
-//   })
-
-//   response.status(200).json(app.locals.reviews)
-// })
-
-// //PUT request:  Complete button
-// app.put('/reviews/complete/:id', (request, response) => {
-
-//   const { id } = request.params;
-
-//   app.locals.reviews.forEach((review) => {
-//     if(parseInt(id) === review.id) {
-//       review.status = 'complete';
-//     }
-//   })
-
-//   response.status(200).json(app.locals.reviews)
-// })
-
-
-// //PUT request:  ACCEPT button
-// app.put('/reviews/accept/:id/:user', (request, response) => {
-
-//   const { id, user } = request.params;
-
-//   app.locals.reviews.forEach((review) => {
-//     if(parseInt(id) === review.id) {
-//       review.status = 'active';
-//       review.reviewer = `${user}`;
-//     }
-//   })
-
-//   response.status(200).json(app.locals.reviews)
-// })
-
-
-// app.listen(app.get('port'), () => {
-//   console.log(`Running on port ${app.get('port')}`)
-// })
